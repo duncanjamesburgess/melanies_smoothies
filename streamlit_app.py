5d4912bcc667d85f8aa88c4eb46d12a226888f7d
duncanjamesburgess <145762392+duncanjamesburgess@users.noreply.github.com>
2024-09-21 08:55:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functons import col
-
+from snowflake.snowpark.functions import col
 # Write directly to the app
 streamlit.title("My Parenets New Healthy Diner")
 st.write(
